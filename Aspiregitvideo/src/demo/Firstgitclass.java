416bc237eb7077a2d90e20254c517c6fa7a8c537
fiza <fiza@LAPTOP-AS2IU8D8>
2022-02-21 11:49:51 +0400

    GitvideolectureFirstcommit

---
+++ Aspiregitvideo/src/demo/Firstgitclass.java
@@ -0,0 +1,10 @@
+package demo;
+
+
+
+public class Firstgitclass {
+	public static void main(String[] args) {
+		System.out.println("Thuis is demo class for git video lecture. Checking how git connection with eclipse works.");
+	}
+
+}
