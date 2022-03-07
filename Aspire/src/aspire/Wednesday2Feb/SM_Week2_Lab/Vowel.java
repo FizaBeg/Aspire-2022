8050cbc3abedd97b80b51ec654a652e9939db078
fiza <fiza@LAPTOP-AS2IU8D8>
2022-03-07 09:42:05 +0400

    Character check whether it is vowel or not

---
+++ Aspire/src/aspire/Wednesday2Feb/SM_Week2_Lab/Vowel.java
@@ -0,0 +1,15 @@
+package aspire.Wednesday2Feb.SM_Week2_Lab;
+
+public class Vowel {
+	public static void main(String[] args) {
+		char ch = 'i';
+		
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') 
+			System.out.println(ch + " is vowel");
+		else
+			System.out.println(ch + " is consonent");
+			
+		}
+	}
+
+
