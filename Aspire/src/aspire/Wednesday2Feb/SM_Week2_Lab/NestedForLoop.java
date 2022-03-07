91143f3410d48d877a779131d7de08e0a9c2f4ac
fiza <fiza@LAPTOP-AS2IU8D8>
2022-03-07 09:56:44 +0400

    Java nested for loop

---
+++ Aspire/src/aspire/Wednesday2Feb/SM_Week2_Lab/NestedForLoop.java
@@ -0,0 +1,10 @@
+package aspire.Wednesday2Feb.SM_Week2_Lab;
+
+public class NestedForLoop {
+	public static void main(String[] args) {
+		for(int i =0; i<=3; i++)
+			for(int j=0; j<=3; j++)
+				System.out.println(i + " " + j);
+	}
+
+}
