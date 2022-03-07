0557d82e3d54510d8bdb6ed4c748c49d8838a94e
fiza <fiza@LAPTOP-AS2IU8D8>
2022-03-07 11:00:02 +0400

    Do..While Loop

---
+++ Aspire/src/aspire/Wednesday2Feb/SM_Week2_Lab/DoWhileLoo.java
@@ -0,0 +1,13 @@
+package aspire.Wednesday2Feb.SM_Week2_Lab;
+
+public class DoWhileLoo {
+	 public static void main(String args[]) {
+	 int x = 1;
+
+	 do {
+	 System.out.print("value of x : " + x );
+	 x++;
+	 System.out.print("\n");
+	 }while( x < 11 );
+	 }
+	}
