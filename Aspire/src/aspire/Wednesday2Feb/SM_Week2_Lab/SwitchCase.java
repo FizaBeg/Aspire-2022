641200fb433c369b4e88d94c19d1a490530e45d4
fiza <fiza@LAPTOP-AS2IU8D8>
2022-03-07 11:50:37 +0400

    Switch Case

---
+++ Aspire/src/aspire/Wednesday2Feb/SM_Week2_Lab/SwitchCase.java
@@ -0,0 +1,28 @@
+package aspire.Wednesday2Feb.SM_Week2_Lab;
+
+public class SwitchCase {
+	public static void main(String[] args) {
+		int tech = 2;
+		
+		switch(tech) {
+		case 1: 
+		{
+			System.out.println("java");
+			break;
+		}
+		case 2:
+		{
+			System.out.println("ES6");
+			break;
+			
+		}
+		default:
+			System.out.println("Not listed ...");
+			
+			
+		}
+		
+			
+	}
+
+}
