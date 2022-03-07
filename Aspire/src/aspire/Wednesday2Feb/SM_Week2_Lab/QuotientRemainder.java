854b2bc67cce7e0d69f0d862c1cc6fdd76c93570
fiza <fiza@LAPTOP-AS2IU8D8>
2022-03-07 08:24:00 +0400

    Computer Quotient and Remainder

---
+++ Aspire/src/aspire/Wednesday2Feb/SM_Week2_Lab/QuotientRemainder.java
@@ -0,0 +1,16 @@
+package aspire.Wednesday2Feb.SM_Week2_Lab;
+
+public class QuotientRemainder {
+	public static void main(String[] args) {
+		
+		int dividend = 25, divisor = 4;
+		
+		int quotient = dividend/divisor;
+		int remainder = dividend%divisor;
+		
+		System.out.println("The dividend is : " + dividend);
+		System.out.println("The divisor is : " + divisor);
+		System.out.println("The quotient is : " + quotient);
+		System.out.println("The reaminder is : " + remainder);
+	}
+}
