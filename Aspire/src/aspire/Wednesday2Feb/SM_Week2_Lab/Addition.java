f96d815f5ae3e0e74926229bd9a4dd2f707e71f4
fiza <fiza@LAPTOP-AS2IU8D8>
2022-03-07 07:46:45 +0400

    Adittion

---
+++ Aspire/src/aspire/Wednesday2Feb/SM_Week2_Lab/Addition.java
@@ -0,0 +1,14 @@
+package aspire.Wednesday2Feb.SM_Week2_Lab;
+
+public class Addition {
+	public static void main(String[] args) {
+		System.out.println("Enter two numbers: ");
+		int first = 100;
+		int second = 200;
+		System.out.println("First : " + first + "\t" + "Second : " + second);
+		//add two numbers
+		int sum = first + second;
+		System.out.println("Sum of the two number is : " + sum); 
+	}
+
+}
