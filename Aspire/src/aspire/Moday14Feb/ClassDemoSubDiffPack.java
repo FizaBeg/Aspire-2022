68de7fe6bb9ed4d95fdf3f99a4ffb086005daf99
fiza <fiza@LAPTOP-AS2IU8D8>
2022-03-15 23:41:43 +0400

    class demo

---
+++ Aspire/src/aspire/Moday14Feb/ClassDemoSubDiffPack.java
@@ -0,0 +1,11 @@
+package aspire.Moday14Feb;
+
+import aspire.Moday14Feb.ClassDemo1;
+public class ClassDemoSubDiffPack extends ClassDemo1{//ch6.ClassDemo1{//qname
+	void met() {
+		//System.out.println(pri);//- CLASS SCOPE
+		//System.out.println(nomod);//package scope
+		System.out.println(pro);//relation scope - top u have inheritance
+		System.out.println(pub);
+	}
+}
