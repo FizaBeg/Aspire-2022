5aceef5337a42504c7988428c1014681200d269f
fiza <fiza@LAPTOP-AS2IU8D8>
2022-03-16 06:55:50 +0400

    argument

---
+++ Aspire/src/aspire/Moday28Feb/ArgsDemo.java
@@ -0,0 +1,9 @@
+package aspire.Moday28Feb;
+
+//https://fluvid.com/videos/detail/mv1dViwBadI7xeG3p#.Yhw3w01eLl8.link
+public class ArgsDemo {
+	public static void main(String[] args) {
+		System.out.println(args[0]);
+		System.out.println(args[4]);
+	}
+}
